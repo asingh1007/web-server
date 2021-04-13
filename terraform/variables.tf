64b06e01140dfe09ff444f0b904ccfbe8e93056b
Amandeep Chadha <amandeep.chadha@olx.com>
2021-04-13 21:44:10 +0530

    adding docker build and push action

---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "project_id" {  
+    default="test-aman-1007"
+}
+variable "region" {
+  default = "us-central1"
+}
+variable "ip_range_pods" {
+    default = "ip_range_pods"
+}
+variable "ip_range_services" {
+    default = "ip_range_services"
+}
+
+variable "bucket_name" {
+  default  = "bucket_name"
+}
